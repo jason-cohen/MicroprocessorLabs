b4fe512f0c09c74576fc6e1401d7158db2fc8d3d
jason-cohen <jason.cohen@mail.mcgill.ca>
2013-03-19 15:17:24 -0400

    Demo code for Lab4

--- Lab4/DMA.c
+++ Lab4/DMA.c
@@ -1,244 +1,58 @@
 
 #include "DMA.h"
 
-__IO TestStatus  TransferStatus = FAILED;
-// const uint32_t SRC_Const_Buffer[BUFFER_SIZE]= {
-//                                     0x01020304,0x05060708,0x090A0B0C,0x0D0E0F10,
-//                                     0x11121314,0x15161718};
-uint32_t DST_Buffer[BUFFER_SIZE];
-uint32_t spi1TxBuffer[BUFFER_SIZE + 1] = {LIS302DL_OUT_X_ADDR, 
+int8_t txBuffer[BUFFER_SIZE] = {LIS302DL_OUT_X_ADDR|0x40|0x80, 
 															DUMMY_BYTE, DUMMY_BYTE, 
 															DUMMY_BYTE, DUMMY_BYTE, 
 															DUMMY_BYTE, DUMMY_BYTE};
 
-uint32_t spiRxBuffer[BUFFER_SIZE];
+int8_t rxBuffer[BUFFER_SIZE] = {DUMMY_BYTE,
+ 															DUMMY_BYTE, DUMMY_BYTE, 
+ 															DUMMY_BYTE, DUMMY_BYTE, 
+ 															DUMMY_BYTE, DUMMY_BYTE};
 
 void DMA_init() {
-	//program initiates SPI1 and confirms that polling works.
-	LIS302DL_CS_HIGH(); //GPIO_SetBits(GPIOA, GPIO_Pin_4); //Set CS Off
-	SPI_Cmd(SPI1, DISABLE);
 	 
-	uint16_t SPI_MASTER_Buffer_Rx[BUFFER_SIZE] = {99,99,99,99,99};
-	uint16_t SPI_MASTER_Buffer_Tx[BUFFER_SIZE] = {0x8000, 0, 0x7E00,  0x8003,  0};
-	 
-	RCC_AHB1PeriphResetCmd(RCC_AHB1Periph_DMA2, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 	DMA_Cmd(Rx_DMA, DISABLE);
 	DMA_Cmd(Tx_DMA, DISABLE);
 	 
 	/* SPI_MASTER_Rx_DMA_Channel configuration ---------------------------------*/
-	DMA_DeInit(Rx_DMA);
+	
 	DMA_InitTypeDef DMA_InitStructure;
+	DMA_DeInit(Rx_DMA);
 	DMA_StructInit(&DMA_InitStructure);
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(LIS302DL_SPI->DR); //not sure about this
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)spiRxBuffer;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-	DMA_InitStructure.DMA_BufferSize = (uint32_t)BUFFER_SIZE;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
 	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 	DMA_InitStructure.DMA_Channel = DMA_Channel_3;
 	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
-	//  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+
 	DMA_Init(Rx_DMA, &DMA_InitStructure);
 	 
 	/* SPI_MASTER_Tx_DMA_Channel configuration ---------------------------------*/
 	DMA_DeInit(Tx_DMA);
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(LIS302DL_SPI->DR); //not sure about this
-	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)spi1TxBuffer;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 	DMA_Init(Tx_DMA, &DMA_InitStructure);
 	 
 	DMA_ITConfig(Rx_DMA, DMA_IT_TC, ENABLE);
 	 
-	/* Enable SPI_MASTER DMA Tx request */
-	SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Tx | SPI_I2S_DMAReq_Rx, ENABLE);
-	/* Enable SPI_MASTER DMA Rx request */
-	//SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Rx, ENABLE);
-	 
-	SPI_I2S_ClearFlag(SPI1, SPI_I2S_FLAG_TXE);
-	SPI_I2S_ClearFlag(SPI1, SPI_I2S_FLAG_RXNE);
-	LIS302DL_CS_LOW(); //GPIO_ResetBits(GPIOA, GPIO_Pin_4); //Set CS On
-	/* Enable SPI_MASTER */
-	SPI_Cmd(LIS302DL_SPI, ENABLE);
+	/* Enable the DMA Stream IRQ Channel */
+	initNVIC_DMA2();
 	 
 	/* Enable DMA channels */
-	DMA_Cmd(DMA2_Stream0, ENABLE);
-	DMA_Cmd(DMA2_Stream5, ENABLE);
-	 
-	//while(!DMA_GetFlagStatus(DMA2_Stream0, DMA_FLAG_TCIF0));
-	while(!DMA_GetFlagStatus(DMA2_Stream5, DMA_FLAG_TCIF5));
-	LIS302DL_CS_HIGH(); //GPIO_SetBits(GPIOA, GPIO_Pin_4); //Set CS Off
+// 	DMA_Cmd(Rx_DMA, ENABLE);
+// 	DMA_Cmd(Tx_DMA, ENABLE);
 	
 	
 	
+	SPI_DMACmd(SPI1, SPI_DMAReq_Rx | SPI_DMAReq_Tx, ENABLE); //Start Rx dma on SPI1
 	
-	
-	
-	
-//   __IO uint32_t    Timeout = TIMEOUT_MAX;
-//   DMA_InitTypeDef       DMA_InitStructure;
-
-//   /* Enable DMA clock */
-//   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
-// 	
-// 	/* Reset DMA Stream registers (for debug purpose) */
-//   DMA_DeInit(DMA2_Stream0);
-// 	
-//   /* Check if the DMA Stream is disabled before enabling it.
-//      Note that this step is useful when the same Stream is used multiple times:
-//      enabled, then disabled then re-enabled... In this case, the DMA Stream disable
-//      will be effective only at the end of the ongoing data transfer and it will 
-//      not be possible to re-configure it before making sure that the Enable bit 
-//      has been cleared by hardware. If the Stream is used only once, this step might 
-//      be bypassed. */
-//   while (DMA_GetCmdStatus(DMA2_Stream0) != DISABLE);
-//   
-//   /* Configure DMA Stream */
-// 	DMA_StructInit(&DMA_InitStructure);
-// 	/* DMA2 Stream0 channel0 configuration **************************************/
-//   DMA_InitStructure.DMA_Channel = DMA_Channel_0;  
-//   DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(LIS302DL_SPI->DR);
-//   DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) DST_Buffer;
-//   DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-//   DMA_InitStructure.DMA_BufferSize = BUFFER_SIZE;
-//   DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-//   DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-//   DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-//   DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-//   DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
-//   DMA_InitStructure.DMA_Priority = DMA_Priority_High;
-//   DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;         
-//   DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
-//   DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-//   DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-//   DMA_Init(DMA2_Stream0, &DMA_InitStructure);
-//   DMA_Cmd(DMA2_Stream0, ENABLE);
-
-//   
-// 	/* Enable DMA Stream Transfer Complete interrupt */
-//   DMA_ITConfig(DMA2_Stream0, DMA_IT_TC, ENABLE);
-// 	
-// 	/* Enable SPI1 DMA */
-//   SPI_I2S_DMACmd(LIS302DL_SPI, SPI_I2S_DMAReq_Rx, ENABLE);
-//   /* DMA Stream enable */
-//   DMA_Cmd(DMA2_Stream0, ENABLE);
-
-//   /* Check if the DMA Stream has been effectively enabled.
-//      The DMA Stream Enable bit is cleared immediately by hardware if there is an 
-//      error in the configuration parameters and the transfer is no started (ie. when
-//      wrong FIFO threshold is configured ...) */
-//   Timeout = TIMEOUT_MAX;
-//   while ((DMA_GetCmdStatus(DMA2_Stream0) != ENABLE) && (Timeout-- > 0));
-//    
-//   /* Check if a timeout condition occurred */
-//   if (Timeout == 0) {
-//     /* Manage the error: to simplify the code enter an infinite loop */
-//     while (1);
-//   }
-}
-
-
-/**
-  * @brief  Configure the DMA controller according to the Stream parameters
-  *         defined in main.h file 
-  * @param  None
-  * @retval None
-  */
-void DMA_Config(void)
-{
-  NVIC_InitTypeDef NVIC_InitStructure;
-  DMA_InitTypeDef  DMA_InitStructure;
-  __IO uint32_t    Timeout = TIMEOUT_MAX;
-    
-  /* Enable DMA clock */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
-  
-  /* Reset DMA Stream registers (for debug purpose) */
-  DMA_DeInit(Rx_DMA);
-
-  /* Check if the DMA Stream is disabled before enabling it.
-     Note that this step is useful when the same Stream is used multiple times:
-     enabled, then disabled then re-enabled... In this case, the DMA Stream disable
-     will be effective only at the end of the ongoing data transfer and it will 
-     not be possible to re-configure it before making sure that the Enable bit 
-     has been cleared by hardware. If the Stream is used only once, this step might 
-     be bypassed. */
-  while (DMA_GetCmdStatus(Rx_DMA) != DISABLE);
-  
-  /* Configure DMA Stream */
-	DMA_StructInit(&DMA_InitStructure);
-  DMA_InitStructure.DMA_Channel = DMA_Channel_0;  
-  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(LIS302DL_SPI->DR);
-  DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)DST_Buffer;
-  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-  DMA_InitStructure.DMA_BufferSize = (uint32_t)BUFFER_SIZE;
-  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
-  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
-  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;         
-  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
-  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-  DMA_Init(DMA2_Stream0, &DMA_InitStructure);
-    
-  /* Enable DMA Stream Transfer Complete interrupt */
-  DMA_ITConfig(Rx_DMA, DMA_IT_TC, ENABLE);
-
-  /* DMA Stream enable */
-  DMA_Cmd(Rx_DMA, ENABLE);
-
-  /* Check if the DMA Stream has been effectively enabled.
-     The DMA Stream Enable bit is cleared immediately by hardware if there is an 
-     error in the configuration parameters and the transfer is no started (ie. when
-     wrong FIFO threshold is configured ...) */
-  Timeout = TIMEOUT_MAX;
-  while ((DMA_GetCmdStatus(DMA2_Stream0) != ENABLE) && (Timeout-- > 0))
-  {
-  }
-   
-  /* Check if a timeout condition occurred */
-  if (Timeout == 0)
-  {
-    /* Manage the error: to simplify the code enter an infinite loop */
-    while (1)
-    {
-    }
-  }
-
-  /* Enable the DMA Stream IRQ Channel */
-  NVIC_InitStructure.NVIC_IRQChannel = DMA2_Stream0_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);     
-}
-
-/**
-  * @brief  Compares two buffers.
-  * @param  pBuffer, pBuffer1: buffers to be compared.
-  * @param  BufferLength: buffer's length
-  * @retval PASSED: pBuffer identical to pBuffer1
-  *         FAILED: pBuffer differs from pBuffer1
-  */
-TestStatus Buffercmp(const uint32_t* pBuffer, uint32_t* pBuffer1, uint16_t BufferLength)
-{
-  while(BufferLength--)
-  {
-    if(*pBuffer != *pBuffer1)
-    {
-      return FAILED;
-    }
-    
-    pBuffer++;
-    pBuffer1++;
-  }
-
-  return PASSED;  
 }
--- Lab4/main.c
+++ Lab4/main.c
@@ -9,17 +9,35 @@
 #include "LEDs.h"
 #include "button.h"
 #include "interrupts.h"
+#include "DMA.h"
+
+#define ACC_SAMPLE_FLAG 0x01
+#define TEMP_SAMPLE_FLAG 0x01	
+
+#define USE_DMA 1
 
 /* Variable declarations */
 
 float alphaAngle = 0.0;					// Roll angle, x axis and horizontal plane
 float betaAngle = 0.0;					// Pitch angle, y axis and horizontal plane
 
+float alphaRad = 0.0;					// Roll angle, x axis and horizontal plane
+float betaRad = 0.0;					// Pitch angle, y axis and horizontal plane
+
 float xOffset = 0.0;						// Gravity offset for calculating horizontal movement
 float yOffset = 0.0;						// Gravity offset for calculating horizontal movement
 
 float xAccel = 0.0;							// Summation of all x direction accelerations
 float yAccel = 0.0;							// Summation of all y direction accelerations
+
+// int8_t txBuffer[BUFFER_SIZE] = {LIS302DL_OUT_X_ADDR|0x40|0x80, 
+// 															DUMMY_BYTE, DUMMY_BYTE, 
+// 															DUMMY_BYTE, DUMMY_BYTE, 
+// 															DUMMY_BYTE, DUMMY_BYTE};
+// int8_t rxBuffer[BUFFER_SIZE] = {DUMMY_BYTE,
+// 															DUMMY_BYTE, DUMMY_BYTE, 
+// 															DUMMY_BYTE, DUMMY_BYTE, 
+// 															DUMMY_BYTE, DUMMY_BYTE};
 
 // Temperature measurement modulo 8 for use with the board LEDs
 uint8_t tempModulo = 0;
@@ -28,8 +46,6 @@ uint8_t tempModulo = 0;
 movAvgState movAvgStateStruct[NUM_AXES];
 movAvgState movAvgTemperature;
 
-// ID for thread
-osThreadId tid_threadTempSense, tid_threadACC;
 // Select tap mode
 uint8_t tapSelection = 0;
 // Select button mode
@@ -38,6 +54,32 @@ uint8_t buttonSelection = 0;
 uint8_t buttonPressed = 0;
 // Scale factor so that ADC polls at 20Hz instead of 100Hz
 uint8_t ADCIntCount = 5;
+// Flag to be set when DMA has complete transfer
+uint8_t dmaDataReady = 0;
+															
+// Semaphore definitions
+osSemaphoreDef(temp_s)
+osSemaphoreDef(acc_s)
+osSemaphoreId temp_sid;
+osSemaphoreId acc_sid;
+
+/*!
+ @brief Thread to perform temperature measurements
+ @param argument Unused
+ */
+void threadTempSense(void const * argument);
+/*!
+ @brief Thread to perform accelerometer measurements
+ @param argument Unused
+ */
+void threadACC(void const *argument);
+
+//! Thread structure for above thread
+osThreadDef(threadTempSense, osPriorityNormal, 1, 0);
+osThreadDef(threadACC, osPriorityNormal, 1, 0);
+// ID for threads
+osThreadId tid_threadTempSense;
+osThreadId tid_threadACC;
 
 /**
  * Converts the ADC data into a temperature measurement
@@ -60,25 +102,15 @@ void calculateAngles(void);
 void determineMovement(void);
 
 /*!
- @brief Thread to perform menial tasks such as switching LEDs
- @param argument Unused
- */
- 
-void threadTempSense(void const * argument);
-void threadACC(void const *argument);
-
-//! Thread structure for above thread
-osThreadDef(threadTempSense, osPriorityNormal, 1, 0);
-osThreadDef(threadACC, osPriorityNormal, 1, 0);
-
-/*!
  @brief Program entry point
  */
 int main (void) {
 	uint8_t i;
 	
-	// Run setups based on debugging settings
-
+	// Semaphore initialization
+	temp_sid = osSemaphoreCreate(osSemaphore(temp_s), 1);
+	acc_sid = osSemaphoreCreate(osSemaphore(acc_s), 1);
+	
 	initGPIOD_LED();
 	initGPIOA_BUTTON();
 
@@ -88,7 +120,9 @@ int main (void) {
   
 	initAcc();
 	temperatureSensorInit();
-	
+	#if USE_DMA
+	DMA_init();
+	#endif
 // Configuration for setting up the tapping interrupt.
 	initEXTI0();
 	initNVIC_EXTI0();
@@ -142,10 +176,10 @@ int main (void) {
 			}
 		} else if (tapSelection && !buttonSelection) {
 			// display ACC movement LEDs
-			if (xAccel > 0.01) {
+			if (xAccel > 0.005) {
 				GPIO_SetBits(GPIOD, LD6);
 				GPIO_ResetBits(GPIOD, LD3);
-			} else if (xAccel < -0.01) {
+			} else if (xAccel < -0.005) {
 				GPIO_SetBits(GPIOD, LD3);
 				GPIO_ResetBits(GPIOD, LD6);
 			} else {
@@ -153,10 +187,10 @@ int main (void) {
 				GPIO_ResetBits(GPIOD, LD6);
 			}
 			
-			if (yAccel > 0.01) {
+			if (yAccel > 0.005) {
 				GPIO_SetBits(GPIOD, LD5);
 				GPIO_ResetBits(GPIOD, LD4);
-			} else if (yAccel < -0.01) {
+			} else if (yAccel < -0.005) {
 				GPIO_SetBits(GPIOD, LD4);
 				GPIO_ResetBits(GPIOD, LD5);
 			} else {
@@ -197,7 +231,7 @@ int main (void) {
 void threadTempSense (void const *argument) {
 	while(1){
 		//Toggled at a rate of 20hz
-		osSignalWait(2,osWaitForever);
+		osSignalWait(TEMP_SAMPLE_FLAG,osWaitForever);
 		ADC_SoftwareStartConv(ADC1);
 
 		while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET);
@@ -205,25 +239,79 @@ void threadTempSense (void const *argument) {
 		
 		// Calculate the temperature using conversions
 		calculateTemperature();
-		
+		//osSignalClear(tid_threadTempSense, TEMP_SAMPLE_FLAG); //Clear the sample flag
 	}
 }
 
 void threadACC(void const *argument) {
+	#if USE_DMA
+	uint32_t i = 0;
+	
+	LIS302DL_CS_LOW(); //Lower CS line
+
+	//DMA2_Stream0->M0AR = (uint32_t)rxptr;
+	Rx_DMA->NDTR = BUFFER_SIZE;
+	Rx_DMA->M0AR = (uint32_t)rxBuffer;
+
+	//DMA2_Stream3->M0AR = (uint32_t)txptr;
+	Tx_DMA->NDTR = BUFFER_SIZE;
+	Tx_DMA->M0AR = (uint32_t)txBuffer;
+
+  DMA_Cmd(Rx_DMA, ENABLE);
+	DMA_Cmd(Tx_DMA, ENABLE);
+	
+	#endif
 	while (1) {
 		//Toggled at a rate of 100hz
-		osSignalWait(1,osWaitForever);
-		
-		//We read from the accelerometer to obtain the raw data.
-		LIS302DL_ReadACC(accData);
-		
-		//We calibrate the data (filters the values)
-		calibrateData();
-		
-		//Calculate the angles using trig
-		calculateAngles();
-		
-		determineMovement();
+		osSignalWait(ACC_SAMPLE_FLAG,osWaitForever);
+		#if USE_DMA
+		if (dmaDataReady) {
+			//osSemaphoreWait(acc_sid, osWaitForever); //Have exclusive access to temperature
+			
+			int32_t* out = &accData[0];
+			// Scale the values from DMA to the actual values
+			// This was done in LIS302DL_ReadACC() when using the LIS302 driver
+			for(i=0; i<0x03; i++) {
+					*out =(int32_t)(18 *  (int8_t)rxBuffer[2*i +1]);
+					out++;
+			}
+		#endif
+		#if !USE_DMA
+			LIS302DL_ReadACC(accData);
+		#endif
+			//We calibrate the data (filters the values)
+			calibrateData();
+			
+			//Calculate the angles using trig
+			calculateAngles();
+			
+			determineMovement();
+			
+			//osSemaphoreRelease(acc_sid);
+			#if USE_DMA
+			// Setup DMA to run again
+			LIS302DL_CS_LOW(); //Lower CS line
+			
+// 			DMA_Cmd(Rx_DMA, DISABLE);
+// 			DMA_Cmd(Tx_DMA, DISABLE);
+
+			//DMA2_Stream0->M0AR = (uint32_t)rxptr;
+			Rx_DMA->M0AR = (uint32_t)rxBuffer;
+			Tx_DMA->M0AR = (uint32_t)txBuffer;
+			
+			Rx_DMA->NDTR = BUFFER_SIZE;
+			Tx_DMA->NDTR = BUFFER_SIZE;
+			
+			DMA_Cmd(Rx_DMA, ENABLE);
+			DMA_Cmd(Tx_DMA, ENABLE);
+			
+			
+			
+			dmaDataReady = 0; //Clear DMA flag0
+			//osSignalClear(tid_threadACC, ACC_SAMPLE_FLAG); //Clear the sample flag
+		}
+		#endif
+		osSignalClear(tid_threadACC, ACC_SAMPLE_FLAG);
 	}
 }
 
@@ -246,28 +334,36 @@ void calculateAngles() {
 		movingAverage(&movAvgStateStruct[i], calibratedData[i]);
 	}
 	
-	xOffset = atan2(movAvgStateStruct[0].avg, sqrt(movAvgStateStruct[1].avg*movAvgStateStruct[1].avg + movAvgStateStruct[2].avg*movAvgStateStruct[2].avg));
-	yOffset = atan2(movAvgStateStruct[1].avg, sqrt(movAvgStateStruct[0].avg*movAvgStateStruct[0].avg + movAvgStateStruct[2].avg*movAvgStateStruct[2].avg));
-	alphaAngle = xOffset * 180.0 / PI;	// atan(x / sqrt(y^2 + z^2))
-	betaAngle = yOffset * 180.0 / PI;	// atan(y / sqrt(x^2 + z^2))
+	alphaRad = atan2(movAvgStateStruct[0].avg, sqrt(movAvgStateStruct[1].avg*movAvgStateStruct[1].avg + movAvgStateStruct[2].avg*movAvgStateStruct[2].avg));
+	betaRad = atan2(movAvgStateStruct[1].avg, sqrt(movAvgStateStruct[0].avg*movAvgStateStruct[0].avg + movAvgStateStruct[2].avg*movAvgStateStruct[2].avg));
+	alphaAngle = alphaRad * 180.0 / PI;	// atan(x / sqrt(y^2 + z^2))
+	betaAngle = betaRad * 180.0 / PI;	// atan(y / sqrt(x^2 + z^2))
 	
-	xOffset = sin(xOffset);
-	yOffset = sin(yOffset);
+	xOffset = arm_cos_f32(PI/2 - alphaRad);
+	yOffset = arm_cos_f32(PI/2 - betaRad);
 }
 
 //Doxygen comments in def.
 void determineMovement() {
-	xAccel += (movAvgStateStruct[0].avg - xOffset);
-	yAccel += (movAvgStateStruct[1].avg - yOffset);
+	float tmp;
+	tmp = movAvgStateStruct[0].avg - xOffset;
+	tmp *= arm_cos_f32(alphaRad);
+	//if (tmp > 0.05)
+		xAccel = tmp;
+	tmp = movAvgStateStruct[1].avg - yOffset;
+	tmp *= arm_cos_f32(betaRad);
+	
+	//if (tmp > 0.05)
+		yAccel = tmp;
 }
 
 // 100Hz interrupt
 void TIM3_IRQHandler() {
   if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) {
     TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-		osSignalSet(tid_threadACC, 1);
+		osSignalSet(tid_threadACC, ACC_SAMPLE_FLAG);
 		if (--ADCIntCount <= 0) {
-			osSignalSet(tid_threadTempSense,2);
+			osSignalSet(tid_threadTempSense,TEMP_SAMPLE_FLAG);
 			ADCIntCount = 5;
 		}
   }
@@ -282,4 +378,25 @@ void EXTI0_IRQHandler() {
 // 		osSignalSet(tid_thread, 1);
 		
 	}
+}
+
+/**
+*@brief An interrupt handler for DMA2_Stream0
+*@retval None
+*/
+void DMA2_Stream0_IRQHandler(void)
+{
+	if (DMA_GetITStatus(Rx_DMA, DMA_IT_TCIF0) != RESET) {
+		DMA_ClearITPendingBit(Rx_DMA,DMA_IT_TCIF0);
+		DMA_ClearFlag(Rx_DMA, DMA_FLAG_TCIF0); //Clear the flag for transfer complete
+		DMA_ClearFlag(Tx_DMA, DMA_FLAG_TCIF3);
+		
+		dmaDataReady = 1;				//Set flag for accelerometer sampling
+
+		LIS302DL_CS_HIGH();  //Raise CS Line
+		
+		DMA_Cmd(Rx_DMA, DISABLE); /// RX
+		DMA_Cmd(Tx_DMA, DISABLE); /// TX
+	}
+	
 }
